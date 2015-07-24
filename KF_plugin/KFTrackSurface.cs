aba7260b65e249b1b61fd04d335a9b6633031624
Lo-FiOrbitalScience <ian.elvar@gmail.com>
2015-07-24 11:14:41 +0100

    Tidied KFTrackSurface

--- KF_plugin/KFTrackSurface.cs
+++ KF_plugin/KFTrackSurface.cs
@@ -4,89 +4,35 @@ using UnityEngine;
 
 namespace KerbalFoundries
 {
-    public class KFTrackSurface : PartModule, IPartSizeModifier
+    public class KFTrackSurface : PartModule
     {
         GameObject _trackSurface;
         KFModuleWheel _track;
+        SkinnedMeshRenderer _smr;
+        Vector2 textureOffset;
         Material trackMaterial = new Material(Shader.Find("Diffuse"));
-        //[KSPField(isPersistant = true)]
-        public static Vector3 initialCraftSize = Vector3.zero;
-        
-        Vector3 attachedCraftSize = Vector3.zero;
 
+        //config fields
         [KSPField]
         public float trackLength = 10;
 
-        public override void OnAwake()
-        {
-            base.OnAwake();
-            Debug.Log("Track Surface Awake");
-            if (HighLogic.LoadedSceneIsEditor)
-            {
-                Debug.Log("Awake and in editor");
-                initialCraftSize = ShipConstruction.CalculateCraftSize(EditorLogic.fetch.ship);
-                Debug.LogError(initialCraftSize);
-            }
-        }
-
-        public Vector3 GetInitialSize()
-        {
-            return initialCraftSize;
-        }
-
-        public void OnEditorAttach()
-        {
-            //initialCraftSize = ShipConstruction.CalculateCraftSize(EditorLogic.fetch.ship);
-            print("editor attach");
-        }
-
-        public void Update()
-        {
-            //print(initialCraftSize);
-        }
-
-        public void onPartAttach()
-        {
-            print("part attach");
-        }
-
-        public Vector3 GetModuleSize(Vector3 defaultSize) //to do with theIPartSizeModifier stupid jiggery.
-        {
-            Vector3 sizeDoesMatter = GetInitialSize();
-            Debug.LogError(sizeDoesMatter + " initial");
-            if (defaultSize.magnitude > attachedCraftSize.magnitude)
-                attachedCraftSize = defaultSize;
-            Debug.LogError(attachedCraftSize + " attach size");
-            Vector3 returnCraftSize = -attachedCraftSize + sizeDoesMatter;
-            Debug.LogError(returnCraftSize + " return");
-            return returnCraftSize;
-        }
 
         public override void OnStart(PartModule.StartState state)
         {
             base.OnStart(state);
             print(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
 
-			foreach (SkinnedMeshRenderer Track in this.part.GetComponentsInChildren<SkinnedMeshRenderer>()) //this is the track
-                _trackSurface = Track.gameObject;
+            _smr = this.part.GetComponentInChildren<SkinnedMeshRenderer>(); //this is the track
+            _trackSurface = _smr.gameObject;
             _track = this.part.GetComponentInChildren<KFModuleWheel>();
-
-            
 
             if (HighLogic.LoadedSceneIsFlight)
             {
                 trackMaterial = _trackSurface.renderer.material;
                 Vector2 trackTiling = trackMaterial.mainTextureScale;
-				Debug.LogWarning(string.Format("Texture tiling is: {0}", trackTiling));
                 trackTiling = new Vector2(trackTiling.x * _track.directionCorrector, trackTiling.y);
-				Debug.LogWarning(string.Format("New texture tiling is: {0}", trackTiling));
                 trackMaterial.SetTextureScale("_MainTex",  trackTiling);
                 trackMaterial.SetTextureScale("_BumpMap", trackTiling);
-            }
-
-            if (HighLogic.LoadedSceneIsEditor)
-            {
-                part.OnEditorAttach += OnEditorAttach;
             }
         }
 
@@ -94,7 +40,7 @@ namespace KerbalFoundries
         {
             base.OnUpdate();
             float distanceTravelled = (float)((_track.averageTrackRPM * 2 * Math.PI) / 60) * Time.deltaTime * _track.directionCorrector; //calculate how far the track will need to move
-            Vector2 textureOffset = trackMaterial.mainTextureOffset;
+            textureOffset = trackMaterial.mainTextureOffset;
             textureOffset = textureOffset + new Vector2(-distanceTravelled / trackLength, 0); //tracklength is used to fine tune the speed of movement.
             trackMaterial.SetTextureOffset("_MainTex", textureOffset);
             trackMaterial.SetTextureOffset("_BumpMap", textureOffset);
