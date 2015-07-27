f9e5ede50a9534272cf8a5d7d8199f651e351aa6
Aqua-KSP <aqua_github@outlook.de>
2015-07-27 17:03:18 +0200

    Fixed part icons again

--- KF_plugin/KFPersistenceManager.cs
+++ KF_plugin/KFPersistenceManager.cs
@@ -344,7 +344,7 @@ namespace KerbalFoundries
 
 			Bounds bounds = boundsList[0];
 			boundsList.RemoveAt(0);
-			boundsList.ForEach(bounds.Encapsulate);
+            boundsList.ForEach(b => bounds.Encapsulate(b)); // Do not change that to boundsList.ForEach(bounds.Encapsulate)!
 
 			return bounds;
 
